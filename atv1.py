1319eb257a68b4bd9faed32f77dd4935f4f11d40
dereksouzasilva-crypto <derek.souza.silva@escola.pr.gov.br>
2025-11-14 10:07:59 -0300

    Create atv1.py

---
+++ atv1.py
@@ -0,0 +1,8 @@
+num = 101
+
+while True:
+    if num % 7 == 0 and num % 3 == 0:
+        print(f"O primeiro número inteiro acima de 100 divisível por 7 e por 3 é: {num}")
+        
+    
+    num += 1
